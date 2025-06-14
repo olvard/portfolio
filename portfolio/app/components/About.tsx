d33095d9c8c697c12238156e3916b353d7192385
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2025-06-14 13:05:38 +0200

    changed bio

--- portfolio/app/components/About.tsx
+++ portfolio/app/components/About.tsx
@@ -10,8 +10,7 @@ const About = () => {
 	
 
         <p className="mt-5 md:max-w-150 font-sans font-light text-justify">
-		Master’s graduate in Media Technology and Engineering with a broad background across software development. Experienced with
-		Python and modern development tools. My education includes work in data visualization, computer graphics, and AI. I
+		Master of Science in Media Technology and Engineering with a broad background across software development. I
 		have a strong interest in emerging technologies and want to use my knowledge to make a diﬀerence. I&apos;m good at working
 		both independently and in teams, contributing positive and curious energy. Excited to apply my skills and competence at
 		a leading company.</p>
